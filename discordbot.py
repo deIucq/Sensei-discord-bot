5ee6a7e16626a7354b26dcddf319a970659a6465
deiucq <kenyoshizoe@gmail.com>
2021-07-28 18:38:13 +0900

    add テスト用起動時，メッセージ受信時処理

--- discordbot.py
+++ discordbot.py
@@ -6,5 +6,19 @@ f.close()
 #クラアントオブジェクトを生成
 client = discord.Client()
 
+#起動時処理
+@client.event
+async def on_ready():
+    print(f'Logged on as {client.user}')
+
+#メッセージ受信時処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 client.run(token)
