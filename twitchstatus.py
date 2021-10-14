672b3dd11c5f516a062552acee023854d4efe504
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-10-14 10:20:16 +0900

    refactor

--- twitchstatus.py
+++ twitchstatus.py
@@ -6,44 +6,43 @@ import requests
 async def twitch_getchannelstatus(client):
     interval = 100
     channelid = ''
-    with open('settings.json') as f:
-        json_dict = json.load(f)
-        interval = json_dict['TwitchAPIInterval']
-        channelid = json_dict['StreamAnnouncementChannelID']
     url = 'https://api.twitch.tv/helix/streams'
     payload = {}
     headers = {}
     with open('settings.json') as f:
         json_dict = json.load(f)
+        interval = json_dict['TwitchAPIInterval']
+        channelid = json_dict['StreamAnnouncementChannelID']
         payload = {'user_login' : json_dict['streamer']}
         headers = {'Client-Id': json_dict["TwitchClientId"], 'Authorization':json_dict["TwitchAuthorization"]}
-
-    online = []
+    #[[{id:, user_login:, game_name:, title:, ...}, msg], [{}, msg]]
+    onlineStream = []
     while True:
+        #APIリクエスト，帰ってきた値をjsonに変換
         data = json.loads(requests.get(url, params=payload, headers=headers).text)
-        offline = []
-        onlineUser_login = []
-        for i in online:
-            onlineUser_login.append(i['user_login'])
-        #onlineをofflineにコピー
-        for i in online:
-            offline.append(i)
+        #onlineStreamの要素のidをonlineStreamsIDsとsfflineStreamsIDsにコピー
+        onlineStreamIDs = []
+        offlineStreamIDs = []
+        for i in onlineStream:
+            onlineStreamIDs.append(i[0]['id'])
+            offlineStreamIDs.append(i[0]['id'])
+        #受けたjsonデータに関する処理
         for i in data['data']:
-            #取得したデータがonlineにない場合通知を送信し，onlineリストにuser_loginとmessageの辞書を追加
-            if i['user_login'] not in onlineUser_login:
+            #取得したデータがonlineStreamIDsにない場合通知を送信し，onlineStreamリストにjsondataとdiscord.messageの配列を追加
+            if i['id'] not in onlineStreamIDs:
                 msgContent = '{}\'s stream goes online \n {}({}) \n https://www.twitch.tv/{}'.format(i['user_login'],i['title'],i['game_name'],i['user_login'])
-                print(msgContent)
                 msg = await client.get_channel(channelid).send(content = msgContent)
-                online.append({'user_login':i['user_login'], 'msg':msg})
-            #取得したデータがofflineにある場合，offlineから取得したデータを削除
+                onlineStream.append([i, msg])
+            #取得したデータがonlineStreamIDsにある場合，offlineから取得したデータを削除
             else:
-                for j in offline:
-                    if i['user_login'] == j['user_login']:
-                        offline.remove(j)
+                for j in offlineStreamIDs:
+                    if i['id'] == j:
+                        offlineStreamIDs.remove(j)
         #残ったofflineについて，onlineからそのデータを削除し，messasgeの内容を書き換える．
-        for i in offline:
-            online.remove(i)
-            msgContent = str(i['user_login'] + "\'s stream goes offline")
-            print(msgContent)
-            await i['msg'].edit(content = msgContent)
+        for i in offlineStreamIDs:
+            for j in onlineStream:
+                if i == j[0]['id']:
+                    msgContent = str(j[0]['user_login'] + "\'s stream goes offline")
+                    await j[1].edit(content = msgContent)
+                    onlineStream.remove(j)
         await asyncio.sleep(interval)
