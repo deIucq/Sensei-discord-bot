95699ff3678f030b3225545425074c5fd0dc7994
Ken Yoshizoe <kenyoshizoe@gmail.com>
2022-03-27 22:11:11 +0900

    delete unused features

--- README.md
+++ README.md
@@ -1,28 +1,15 @@
 # Sensei-discord-bot
 
 ## 概要
-
 discordサーバー「FPS Dojo」用に開発されたdiscord.pyを用いたいろいろできるdiscord botです．
 
-## 主な機能
-### [実装済み]
-- 一時的なサーバーを建てる機能
-- Twitch配信通知機能
-- ランダム機能(VC参加者から一人指名，1から指定した数値までのランダムな値，TRPG等用ダイス)
-
-### [実装予定]
-- ゲームサジェスト機能
-- 参加ゲーム表示機能
-
 ## 実行方法
-
 1. Discord.pyをインストール
-
     ```
     python3 -m pip install -U discord.py
     sudo apt install libffi-dev libnacl-dev python3-dev
     ```
-2. DiscordとTwitchのAPIToken等を入手したらsettings.jsonを生成し，以下の形式で設定
+2. DiscordとTwitchのAPIToken等を入手したらsettings.jsonを作成し，以下の形式で各種設定を行う。
     ```json
     {
     "discordToken":"XXX",
@@ -36,26 +23,29 @@ discordサーバー「FPS Dojo」用に開発されたdiscord.pyを用いたい�
 3. `discordbot.py`を実行
 
 ## コマンド
+1. /twitch
+    - adduser [ユーザー名]
+    - deluser [ユーザー名]
 
+    通知する対象のTwitchユーザーを追加/削除します
+2. /random \
+    引数に数字を入れた場合，1以上その数字以下ののランダムな数字を返します．
+    memberと入れた場合，コマンドを送信した人が参加しているVCの参加者をランダムに選んで返します．
+3. /dice nDm \
+    1以上m以下の値をn回返します
+4. misc
+  - /neko
+  "にゃーん"と返します。botの生存確認に使用してください
+
+削除済み
 1. temp server
   - /mkserver
-    
     サーバー作成
   - /rmserver
-    
     コマンド実行がされたサーバーを削除
   - /liserver
-    
     サーバー一覧
     ✅リアクションで該当サーバーの招待を表示
     🗑️リアクションで該当サーバーを削除
   - /allserverinvite
-    
     botが参加中の全サーバーの招待を返します．
-2. random
-  - /random
-
-    引数に数字を入れた場合，1以上その数字以下ののランダムな数字を返します．
-    memberと入れた場合，コマンドを送信した人が参加しているVCの参加者をランダムに選んで返します．
-  - /dice nDm
-    1以上m以下の値をn回返します
--- discordbot.py
+++ discordbot.py
@@ -4,93 +4,21 @@ import random
 import json
 import twitchstatus
 
-#serversクラスを定義
-class servers():
-    def __init__(self, client):
-        self.sync()
-    def sync(self):
-        self.servers = client.guilds
-    async def mkserver(self, message):
-        f = open('logo/sensei.png', 'rb')
-        im = f.read()
-        f.close()
-        guild = await client.create_guild("temporary server " + str(len(client.guilds)), icon=im)
-        perm = discord.Permissions(administrator=True)
-        await guild.create_role(name='op',permissions=perm)
-        invite = await (await guild.create_text_channel("default")).create_invite()
-        await message.channel.send(invite.url)
-        self.sync()
-    async def rmserver(self, message):
-        await message.guild.delete()
-        self.sync()
-    async def liserver(self, message):
-        self.message = []
-        for i in self.servers:
-            self.message.append(await message.channel.send(i.name))
-        for i in self.message:
-            await i.add_reaction('✅')
-            await i.add_reaction('🗑️')
-    async def reaction(self, reaction):
-        if reaction.emoji == '✅':
-            for i in self.servers:
-                if i.name == reaction.message.content:
-                    if i == reaction.message.guild:continue
-                    invites = await i.invites()
-                    if invites == None:
-                        await reaction.message.channel.send(invites[0].url)
-                    else:
-                        if i.text_channels[0] == None:
-                            invite = await (await i.create_text_channel("default")).create_invite()
-                            await reaction.message.channel.send(invite.url)
-                        else:
-                            invite = await i.text_channels[0].create_invite()
-                            await reaction.message.channel.send(invite.url)
-        elif reaction.emoji == '🗑️':
-            for i in self.servers:
-                if i.name == reaction.message.content:
-                    try:
-                        await i.delete()
-                        await reaction.message.edit(content=i.name + '- deleted')
-                    except discord.HTTPException:
-                        await reaction.message.channel.send(f'I could\'nt delete {i.name}. There\'s some errors.')
-                    except discord.Forbidden:
-                        await reaction.message.channel.send(f'I could\'nt delete {i.name}. I don\'t have permission for this.')
-                    self.sync()
-    async def getallinvite(self, message):
-        for i in client.guilds:
-            if i == message.guild:continue
-            invites = await i.invites()
-            if invites == None:
-                await message.channel.send(invites[0].url)
-            else:
-                if i.text_channels[0] == None:
-                    invite = await (await i.create_text_channel("default")).create_invite()
-                    await message.channel.send(invite.url)
-                else:
-                    invite = await i.text_channels[0].create_invite()
-                    await message.channel.send(invite.url)
-
 client = discord.Client()
-servers = servers(client)
 
 #サーバー参加時処理
 @client.event
 async def on_guild_join(guild):
     print(f'I joined {guild.name} as {client.user}')
     await guild.system_channel.send('Hi.')
-    servers.sync()
 #サーバー脱退時処理
 @client.event
 async def on_guild_remove(guild):
     print(f'I leaved from {guild.name}')
-    servers.sync()
 #起動時処理
 @client.event
 async def on_ready():
     print(f'Logged on as {client.user}')
-    servers.sync()
-    for i in servers.servers:
-        print(i)
     #Twitch通知用の処理を開始
     loop = asyncio.get_event_loop()
     loop.create_task(twitchstatus.twitch_getchannelstatus(client))
@@ -103,19 +31,6 @@ async def on_message(message):
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
-
-    # /mkserver でサーバー作成，招待発行
-    if message.content == '/mkserver':
-        await servers.mkserver(message)
-    # /rmserver でメッセージが発せられたサーバーを削除
-    if message.content == '/rmserver':
-        await servers.rmserver(message)
-    # /liserver で参加中サーバーの一覧
-    if message.content == '/liserver':
-        await servers.liserver(message)
-    # /allserverinvite で全参加中サーバーの招待リンク
-    if message.content == '/allseverinvite':
-        await servers.getallinvite(message)
     # /getop
     if message.content == '/getop':
         if message.guild.owner.id == client.user.id:
@@ -156,9 +71,6 @@ async def on_reaction_add(reaction, user):
     #reactionの主が自分自身だった場合は無視
     if user == client.user:
         return
-    #reactionされたmessageがservers.messageの中にあったら処理を行う
-    if reaction.message in servers.message:
-        await servers.reaction(reaction)
 
 #tokenファイルからtokenを取得
 token = json.load(open('settings.json'))["discordToken"]
